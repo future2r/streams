75ccbaac063b7fddea7f44ee3c2f97db79f6a390
Frank Ulbricht <frank@ulbricht.name>
2019-03-14 13:27:31 +0100

    Added powers-of-two iterator

---
+++ src/main/java/name/ulbricht/streams/impl/source/PowersOfTwo.java
@@ -0,0 +1,22 @@
+package name.ulbricht.streams.impl.source;
+
+import static name.ulbricht.streams.api.StreamOperationType.SOURCE;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import name.ulbricht.streams.api.StreamOperation;
+
+@StreamOperation(name = "Powers of Two", type = SOURCE, output = Long.class)
+public final class PowersOfTwo implements Supplier<Stream<Long>> {
+
+	@Override
+	public Stream<Long> get() {
+		return Stream.iterate(1L, n -> n < (1L << 62), n -> n << 1);
+	}
+
+	@Override
+	public String toString() {
+		return "Stream.iterate(1L, n -> n < (1L << 62), n -> n << 1)";
+	}
+}
